a32843b83fdbf8711de7cbdca4f667b65710f600
Shubhamskp003 <123391547+Shubhamskp003@users.noreply.github.com>
2026-01-24 11:49:40 +0530

    [Shubham] Added: UC2: Render OOPS as Banner using Print Statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,78 @@
+
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
  *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
+ * This class demonstrates a visual banner displaying the word "OOPS" using ASCII art
+ * composed of asterisks (*) and spaces. The banner is printed to the console using
+ * individual print statements for each line, creating a decorative text.
  *
  * @author Developer
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
-
+    /*
+        Main method to run banner display
+        Print each line of the OOPS banner to create visual effect
+        Using individual print statements for each line
+    */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(
+                "*********" + "  " +
+                        "*********" + "  " +
+                        "*********" + "  " +
+                        "*********"
+        );
+
+        // Line 2
+        System.out.println(
+                "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*        "
+        );
+
+        // Line 3
+        System.out.println(
+                "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*        "
+        );
+
+        // Line 4
+        System.out.println(
+                "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*********" + "  " +
+                        "*********"
+        );
+
+        // Line 5
+        System.out.println(
+                "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*        " + "  " +
+                        "        " + "*"
+        );
+
+        // Line 6
+        System.out.println(
+                "*       *" + "  " +
+                        "*       *" + "  " +
+                        "*" + "  " +
+                        "                " + "*"
+        );
+
+        // Line 7
+        System.out.println(
+                "*********" + "  " +
+                        "*********" + "  " +
+                        "*        " + "  " +
+                        "*********"
+        );
     }
 }
+
+
